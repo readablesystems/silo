0393f05629896ed3f9de28b858d4b159e759950b
Nathaniel Herman <nherman@college.harvard.edu>
2014-11-02 17:15:48 -0500

    change colors, allow for black and white

--- plot.py
+++ plot.py
@@ -4,8 +4,24 @@ import numpy
 #import matplotlib
 from collections import OrderedDict
 
+tableau20 = None
+
+COLORZ = True
+
 def settings():
-    pass
+    # These are the "Tableau 20" colors as RGB.  
+    global tableau20
+    tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),  
+                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),  
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),  
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),  
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]  
+  
+    # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.  
+    for i in range(len(tableau20)):  
+        r, g, b = tableau20[i]  
+        tableau20[i] = (r / 255., g / 255., b / 255.)
+
 
 def plot(dat, title):
     fig, ax = plt.subplots()
@@ -43,7 +59,10 @@ def plot(dat, title):
     print data
     
     cur_width = 0
-    colors = ['r', 'g', 'b', 'c']
+    if COLORZ:
+        colors = tableau20
+    else:
+        colors = [(1,1,1), (0,0,0), (.5,.5,.5)]
     n_datapoints = len(data.values()[0].values())
     inds = numpy.arange(n_datapoints)
     n_types = len(data.values())
@@ -56,7 +75,8 @@ def plot(dat, title):
 
     ax.set_xticks(inds + width * n_types / 2.0)
     ax.set_xticklabels(labels, size=10)
-    ax.legend(map(lambda x: x[0], bars), data.keys(), ncol=4, loc='upper center')
+    ax.set_ylabel('Throughput relative to our system', rotation=90)
+    ax.legend(map(lambda x: x[0], bars), data.keys(), ncol=4, loc='upper right', prop={'size': 10})
 #loc='lower center', bbox_to_anchor=(.9, .05) )
 
     ax.set_title(title)
@@ -68,4 +88,10 @@ try:
 except:
     title = ''
 
+try:
+    COLORZ = sys.argv[4] != 'bw'
+except:
+    pass
+
+settings()
 plot(sys.argv[1], title).savefig(sys.argv[2])
