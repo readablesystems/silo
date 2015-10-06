1b1f30164009e891cdbc4505eef30e7d15ebc151
Jeevana <jinala@mit.edu>
2015-10-06 17:17:55 -0400

    Changed hashtable buckets size and some logging stuff

--- benchmarks/mbta_wrapper_norm.hh
+++ benchmarks/mbta_wrapper_norm.hh
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <atomic>
 #include "abstract_db.h"
 #include "abstract_ordered_index.h"
 #include "sto/Transaction.hh"
@@ -13,6 +13,19 @@
     throw abstract_db::abstract_abort_exception(); \
   }
 
+#define OP_LOGGING 1
+
+#if OP_LOGGING
+std::atomic<long> mt_get(0);
+std::atomic<long> mt_put(0);
+std::atomic<long> mt_del(0);
+std::atomic<long> mt_scan(0);
+std::atomic<long> mt_rscan(0);
+std::atomic<long> ht_get(0);
+std::atomic<long> ht_put(0);
+std::atomic<long> ht_del(0);
+#endif 
+
 class mbta_wrapper;
 
 class mbta_ordered_index : public abstract_ordered_index {
@@ -22,6 +35,9 @@ public:
   std::string *arena(void);
 
   bool get(void *txn, const std::string &key, std::string &value, size_t max_bytes_read) {
+#if OP_LOGGING
+    mt_get++;
+#endif
     STD_OP({
 	// TODO: we'll still be faster if we just add support for max_bytes_read
 	bool ret = mbta.transGet(key, value);
@@ -35,6 +51,9 @@ public:
       const std::string &key,
       const std::string &value)
   {
+#if OP_LOGGING
+    mt_put++;
+#endif
     // TODO: there's an overload of put that takes non-const std::string and silo seems to use move for those.
     // may be worth investigating if we can use that optimization to avoid copying keys
     STD_OP({
@@ -52,7 +71,10 @@ public:
   }
 
   void remove(void *txn, const std::string &key) {
-    STD_OP(mbta.transDelete<false>(key));
+#if OP_LOGGING
+    mt_del++;
+#endif
+     STD_OP(mbta.transDelete<false>(key));
   }
 
   void scan(
@@ -61,6 +83,9 @@ public:
             const std::string *end_key,
             scan_callback &callback,
             str_arena *arena = nullptr) {
+#if OP_LOGGING
+    mt_scan++;
+#endif    
     mbta_type::Str end = end_key ? mbta_type::Str(*end_key) : mbta_type::Str();
     STD_OP(mbta.transQuery(start_key, end, [&] (mbta_type::Str key, std::string& value) {
           return callback.invoke(key.data(), key.length(), value);
@@ -74,6 +99,9 @@ public:
              scan_callback &callback,
              str_arena *arena = nullptr) {
 #if 1
+#if OP_LOGGING
+    mt_rscan++;
+#endif
     mbta_type::Str end = end_key ? mbta_type::Str(*end_key) : mbta_type::Str();
     STD_OP(mbta.transRQuery(start_key, end, [&] (mbta_type::Str key, std::string& value) {
           return callback.invoke(key.data(), key.length(), value);
@@ -111,6 +139,9 @@ public:
   std::string *arena(void);
 
   bool get(void *txn, const std::string &key, std::string &value, size_t max_bytes_read) {
+#if OP_LOGGING
+    ht_get++;
+#endif    
     STD_OP({
 	// TODO: we'll still be faster if we just add support for max_bytes_read
 	bool ret = ht.transGet(key, value);
@@ -124,6 +155,9 @@ public:
       const std::string &key,
       const std::string &value)
   {
+#if OP_LOGGING
+    ht_put++;
+#endif
     // TODO: there's an overload of put that takes non-const std::string and silo seems to use move for those.
     // may be worth investigating if we can use that optimization to avoid copying keys
     STD_OP({
@@ -141,6 +175,9 @@ public:
   }
 
   void remove(void *txn, const std::string &key) {
+#if OP_LOGGING
+    ht_del++;
+#endif    
     STD_OP(ht.transDelete(key));
   }
 
@@ -173,7 +210,7 @@ public:
     throw 2;
   }
 
-  typedef Hashtable<std::string, std::string, READ_MY_WRITES/*opacity*/> ht_type;
+  typedef Hashtable<std::string, std::string, READ_MY_WRITES/*opacity*/, 10000000> ht_type;
 private:
   friend class mbta_wrapper;
   ht_type ht;
@@ -202,7 +239,17 @@ public:
 #if PERF_LOGGING
     Transaction::print_stats();
     //    printf("v: %lu, k %lu, ref %lu, read %lu\n", version_mallocs, key_mallocs, ref_mallocs, read_mallocs);
+   {
+        using thd = threadinfo_t;
+        thd tc = Transaction::tinfo_combined();
+        printf("total_n: %llu, total_r: %llu, total_w: %llu, total_searched: %llu, total_aborts: %llu (%llu aborts at commit time)\n", tc.p(txp_total_n), tc.p(txp_total_r), tc.p(txp_total_w), tc.p(txp_total_searched), tc.p(txp_total_aborts), tc.p(txp_commit_time_aborts));
+    }
+
 #endif
+
+#if OP_LOGGING
+    printf("mt_get: %ld, mt_put: %ld, mt_del: %ld, mt_scan: %ld, mt_rscan: %ld, ht_get: %ld, ht_put: %ld, ht_del: %ld\n", mt_get.load(), mt_put.load(), mt_del.load(), mt_scan.load(), mt_rscan.load(), ht_get.load(), ht_put.load(), ht_del.load());
+#endif 
     //txn_epoch_sync<Transaction>::finish();
   }
 
